b356e8fbf38c59ea34c23f1af0a6e9bef07d2227
Ekaterina-11 <62566219+Ekaterina-11@users.noreply.github.com>
2020-04-10 17:44:10 +0400

    Update Product.h

--- Product.h
+++ Product.h
@@ -22,13 +22,13 @@ public:
     Product& operator -- (); // префиксный декремент
     Product operator ++ (int); //постфиксный инкремент
     Product operator -- (int); // постфиксный декремент
-    int operator == (Product product11);//создание оператора равенства
-    int operator != (Product product11);//создание оператора неравенства
-    int operator > (Product product11);//создание оператора больше
-    int operator < (Product product11);//создание оператора меньше
-    int operator >= (Product product11);//создание оператора больше или равно
-    int operator <= (Product product11);//создание оператора меньше или равно
-    Product& operator = (Product product);//создание оператора присваивания
+    int operator == (const Product product11);//создание оператора равенства
+    int operator != (const Product product11);//создание оператора неравенства
+    int operator > (const Product product11);//создание оператора больше
+    int operator < (const Product product11);//создание оператора меньше
+    int operator >= (const Product product11);//создание оператора больше или равно
+    int operator <= (const Product product11);//создание оператора меньше или равно
+    Product& operator = (const Product product);//создание оператора присваивания
 
     friend ostream& operator << (ostream &out, const Product &product_in);//создание оператора вывода
     friend istream& operator >> (istream &in, Product &product_in);//создание оператора ввода
